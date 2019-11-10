a0c30b141e1917d4934c22947e9bf370e0c14693
Seb <webmarken@gmail.com>
2019-11-10 09:19:27 +0200

    improved images

--- components/header.vue
+++ components/header.vue
@@ -3,7 +3,7 @@ header#header.fixed
   .h-full.bg-center.shadow-inner.bg-gray-800.flex.justify-between.px-4.items-center.h-16(class="lg:flex-col lg:justify-around lg:px-0")
     .p-2
       a.flex.content-center.items-center(class="lg:flex-col" href="/")
-        img.shadow-lg.rounded-full.border-2.border-teal-500.w-12(class="lg:w-32 lg:mx-auto" src="~/assets/images/derkinzi.jpg" :srcset="require('~/assets/images/derkinzi.jpg').srcSet")
+        img.shadow-lg.rounded-full.border-2.border-teal-500.w-12(class="lg:w-32 lg:mx-auto" src="~/assets/images/derkinzi.jpg" sizes="10vw" :srcset="require('~/assets/images/derkinzi.jpg').srcSet")
         .font-mono.text-orange-500.ml-2(class="lg:text-2xl lg:text-center lg:mt-4 lg:inline-block lg:ml-0") [derkinzi]
 
     button.block.text-teal-500(@click='isOpen = !isOpen' type='button' class='lg:hidden hover:text-white focus:text-white focus:outline-none')
@@ -17,13 +17,13 @@ header#header.fixed
 
     .social-links.flex.mt-8.px-8.justify-center.hidden(class="lg:flex")
       a.h-8.w-8.rounded-full.p-2.border.border-teal-500.m-1(href='https://instagram.com/derkinzi' target='_blank')
-        img(src="/images/instagram.svg")
+        img(src="~/assets/images/instagram.svg")
       a.h-8.w-8.rounded-full.p-2.border.border-teal-500.m-1(href='https://twitter.com/derkinzi' target='_blank')
-        img(src="/images/twitter.svg")
+        img(src="~/assets/images/twitter.svg")
       a.h-8.w-8.rounded-full.p-2.border.border-teal-500.m-1(href='https://github.com/kinzi' target='_blank')
-        img(src="/images/github.svg")
+        img(src="~/assets/images/github.svg")
       a.h-8.w-8.rounded-full.p-2.border.border-teal-500.m-1(href='https://www.linkedin.com/in/sebastian-kinzlinger-23b86a2b/?originalSubdomain=de' target='_blank')
-        img(src="/images/linkedin.svg")
+        img(src="~/assets/images/linkedin.svg")
 
   .mobile-menu.bg-gray-700.z-50.py-4(:class="{'hidden': !isOpen}")
     ul.text-center.px-10.text-teal-500.font-mono.w-full.mb-0(class="lg:inline-block" :class="{'hidden': !isOpen}")
@@ -32,13 +32,13 @@ header#header.fixed
 
     .social-links.flex.mt-4.px-8.justify-center(class="lg:flex")
       a.h-8.w-8.rounded-full.p-2.border.border-teal-500.m-1(href='https://instagram.com/derkinzi' target='_blank')
-        img(src="/images/instagram.svg")
+        img(src="~/assets/images/instagram.svg")
       a.h-8.w-8.rounded-full.p-2.border.border-teal-500.m-1(href='https://twitter.com/derkinzi' target='_blank')
-        img(src="/images/twitter.svg")
+        img(src="~/assets/images/twitter.svg")
       a.h-8.w-8.rounded-full.p-2.border.border-teal-500.m-1(href='https://github.com/kinzi' target='_blank')
-        img(src="/images/github.svg")
+        img(src="~/assets/images/github.svg")
       a.h-8.w-8.rounded-full.p-2.border.border-teal-500.m-1(href='https://www.linkedin.com/in/sebastian-kinzlinger-23b86a2b/?originalSubdomain=de' target='_blank')
-        img(src="/images/linkedin.svg")
+        img(src="~/assets/images/linkedin.svg")
 
 
 </template>
