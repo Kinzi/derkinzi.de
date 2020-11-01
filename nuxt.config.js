20e0014c65c5d9c6fcc955268a23e5a74bc8cb82
Seb <webmarken@gmail.com>
2020-11-01 08:34:46 +0200

    add matomo

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,6 +80,20 @@ export default async () => {
           type: 'text/javascript',
           innerHTML:
             "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-49352822-2');"
+        },
+        {
+          type: 'text/javascript',
+          innerHTML: `var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//matomo.webmarken.com/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();`
         }
       ]
     },
